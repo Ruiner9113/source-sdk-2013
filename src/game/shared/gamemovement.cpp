512f1dca98ce7ffbbf742f1d45f878680244fa25
Ruiner <84989464+Ruiner9113@users.noreply.github.com>
2026-07-03 13:55:00 -0300

    changed default fov and added auto bhop

--- src/game/shared/gamemovement.cpp
+++ src/game/shared/gamemovement.cpp
@@ -2402,9 +2402,6 @@ bool CGameMovement::CheckJumpButton( void )
 		return false;
 #endif
 
-	if ( mv->m_nOldButtons & IN_JUMP )
-		return false;		// don't pogo stick
-
 	// Cannot jump will in the unduck transition.
 	if ( player->m_Local.m_bDucking && (  player->GetFlags() & FL_DUCKING ) )
 		return false;
